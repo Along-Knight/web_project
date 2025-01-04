a184ba3b3b4dbcd85882eba21f4d2d0e4d62d8e2
Along <2814983352@qq.com>
2025-01-04 17:33:09 +0800

    frist commit

---
+++ maven-project01/src/main/java/com/along/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.along;
+
+/**
+ * @author 22492
+ * @date 2024/10/29 21:10
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven");
+    }
+}
