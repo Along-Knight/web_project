34396f624a5ab3487620f02afc52008a05299786
Along <2814983352@qq.com>
2025-01-06 13:36:49 +0800

    hot-fix commit

--- maven-project01/src/main/java/com/along/HelloWorld.java
+++ maven-project01/src/main/java/com/along/HelloWorld.java
@@ -9,6 +9,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Maven");
         System.out.println("Hello Maven1");
+        System.out.println("Hello Maven2");
 
     }
 }
