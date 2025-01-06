896f7b2ce30fcbfe2a9f0a63892881402cfbabe8
Along <2814983352@qq.com>
2025-01-06 13:28:35 +0800

    second commit

--- maven-project01/src/main/java/com/along/HelloWorld.java
+++ maven-project01/src/main/java/com/along/HelloWorld.java
@@ -8,5 +8,7 @@ package com.along;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Maven");
+        System.out.println("Hello Maven1");
+
     }
 }
